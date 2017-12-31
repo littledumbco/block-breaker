3db853193789e9128d4234ff17545f6d2aab6074
Nick Atkins <littledumbco@gmail.com>
2017-12-31 10:16:33 +0000

    Basic Flow Added
    Again, simple stuff that I'd learnt from the previous tutorial. Adding in a LevelManager so the user can jump from screen to screen.
    
    In this scenario the general flow is Start > Level 01 > Win/Lose > Start.
    
    Next up is adding in a consistent music player GameObject.

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Creating a basic function to handle loading levels
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	// Creating another basic function for handling quitting the game
+	public void QuitRequest (){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+
+
+}
