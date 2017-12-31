9920e432e6e5a21347b816302794b7d77510f743
Nick Atkins <littledumbco@gmail.com>
2017-12-31 10:58:04 +0000

    Statics & the MusicPlayer
    This was a pretty interesting lesson and update. Essentially I had to create a GameObject that would hold a music file and have that GameObject persist throughout the game.
    
    That part is fairly easy, as you can just add a piece of code that tells it not to destroy itself on load (.DontDestroyOnLoad). However, when you loop back round to the Start menu again, it duplicates the song.
    
    In order to tackle this, I had to create a static variable that enabled the script to keep an eye on the duplicates. In a nutshell, the static variable checks out to see if any 'instance' of the Music Player exists. If it already exists, it simply stops (or destroys) any new Music Player gameobjects that are created.
    
    If for some reason a Music Player doesn't exist, it'll create one.
    
    Nifty thing to learn and something I'll have to experiment with in my-playground.

---
+++ Assets/scripts/MusicPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    // Creating a static variable with the name 'instance' - that instance of MusicPlayer is set to null
+    // aka, it does not exist.
+    static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+
+        // Then using the 'instance' variable to check the condition, so if the MusicPlayer already exists
+        // (as it's been created by the Start scene), then do not create a new one when jumping to another
+        // scene
+        
+        if (instance !=null)
+        {
+            Destroy(gameObject);
+            Debug.Log("Duplicate destroyed.");
+
+        // OR if it doesn't exist for some strange reason, then actually create one to exist.
+        } else
+        {
+            // 'this' is used to 'claim' the instance and say yes, this is the global version of 'instance'
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+
+        // gameObject with a small 'g' refers to the object the script is attached to
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
