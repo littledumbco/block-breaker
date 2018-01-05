09bdfd6ee5f6acb97e3e06c4a173e140254a5194
Nick Atkins <littledumbco@gmail.com>
2018-01-05 15:32:15 +0000

    Mouse, Mathf & Paddles
    With the basic physics in for the ball, now had to turn the attention to the Paddle and placing some restraints on it.
    
    I need to dig into this all a bit more, as I felt a bit distracted learning it. I get the general jist of how I've set up the ability to capture the mouse positioning and the constraints to stop the paddle going off the side, but I probably need to give it a go in the my-playground repo.
    
    Maybe I'll do something to stop a square going off the side of the screen.
    
    More of an update in the next commit,  I think.

---
+++ Assets/scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    // Giving the script access to LevelManager so that I can make sure that LoadLevel is available for collision
+    public LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+
+        // utilising levelManager to load the level 'Win' when the ball hits the box off-screen
+        levelManager.LoadLevel("Finish");
+    }
+}
