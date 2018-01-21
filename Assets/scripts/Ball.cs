7daa50d82a71db6894b367fab8329fd4dff50d1c
Nick Atkins <littledumbco@gmail.com>
2018-01-21 13:00:43 +0000

    Paddle & Ball + Launching
    Back to the Unity tutorials after a bit of a break - sorry brain! Managed to catch up with what I was doing before and have added some quite extensive comments to explain the logic.
    
    Essentially this was about ensuring that  the ball tracked with the paddle, so as the user moved their mouse the ball would be locked to the paddle and would move with it. This meant implementing a few variables to track that information.
    
    First variable was about (paddleToBallVector) was about capturing the distance between the ball and the paddle and then subtracting it. I could then use that later within the update function to continously check the ball's position against the paddles and then automaticall subtracting the distance so it was always locked.
    
    As for launching the ball, it was all about applying velocity when a mouse down tap was registered. The issue that the tutorial covers is where the ball-to-paddle tracking is in the update function - so even when you launch the ball, it wouldn't display simply because the update was consistenly removing the distance.
    
    So, the trick to solve this was to create a new bool variable that checked if the game had started vs if it hadn't. If the game hadn't started, then the ball-to-paddle element would consistently run. If, however, the mouse was tapped and therefore started the game then it would run the velocity command.
    
    Next up I assume it will be about controlling the boundaries of where the ball can fly (as currently it goes off screen!).

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // enabling the ability for the Ball script to access the paddle script
+    public Paddle paddle;
+
+    // This has to be added as in the update function the ball is consistently being told
+    // to stick to the paddle. The variable below is to check if the game has started or
+    // not.
+    private bool hasStarted = false;
+
+    // Setting up a private variable to capture the distance between the ball and the paddle
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        // By getting the position of the ball and subtracting that by the position of the paddle
+        // So in this scenario, the ball might be y 0.5 and the paddle might be y 0 - so it takes away 0.5
+        // which puts the ball at 0.
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        // So this is the hasStarted variable implemented to handle that ball stickyness to paddle issue!
+        // IF the game hasn't started then lock the ball to the paddle.
+        if (!hasStarted)
+        {
+            // So this takes the ball's current position, looks at the paddle's position and then adds the distance
+            // missing. So it checks the ball and the paddle every frame and then just removes the distance factor
+            // by applying the paddleToBallVector.
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Mouse clicked, launch ball!");
+
+                // IF the game has started, then let the ball launch.
+                hasStarted = true;
+                // Had to change this up here compared to the tutorial as it had been desolved in newer version
+                // of Unity. 
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
+	}
+}
--- Assets/scripts/Paddle.cs
+++ Assets/scripts/Paddle.cs
@@ -12,10 +12,20 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        // creating a new vector variable called paddlePos that gives it an x position of 0.5 (left side of screen)
+        // takes the y position of the paddle and the z position remains at 0.
         Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 
+        // creates a new number to capture (float) called mousPosInBlocks) that takes the current x position of the
+        // mouse position, divides it by the width of the Screen and then multiples it by 16. This basically converts
+        // the mouse position into game units.
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+        // from the paddlePos vector, we're taking the x position and applying a range to stop it from going off the
+        // screen. This means that the paddle can't actually go past 0.5 on the left side and 15.5 on the right side.
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+
+        // finally just applying this to the paddle itself.
         this.transform.position = paddlePos;
 
         
